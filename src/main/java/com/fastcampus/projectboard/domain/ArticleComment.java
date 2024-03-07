58fc307fa597aa049b99f19a89e870ced3a793c1
YooHyeok <yjk7454@naver.com>
2024-03-07 21:57:15 +0900

    #25 - 회원 계정 엔티티와 게시글, 댓글에 연관관계 적용
    테이블 필드 수정으로 영향 받은
    `data.sql` 테스트 데이터도 모두 업데이트
    
    하는 김에 게시글 -> 댓글 컬렉션의 정렬을 최신 순으로 적용

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @Getter
-@ToString
+@ToString(callSuper = true) // 부모 필드까지 Tostring 적용
 @Table(indexes = { /* 빠르게 서칭이 가능하게끔 인덱스 설정 */
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
@@ -21,19 +21,22 @@ public class ArticleComment extends AuditingFields {
     @GeneratedValue(strategy = GenerationType.IDENTITY) //MySql의 자동증가값은 IDENTITY방식이다.
     private Long id;
 
-    @Setter @ManyToOne(optional = false) // optional하지않는다.(필수값)
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount; //유저 정보(ID)
+
+    @Setter @ManyToOne(optional = false) // optional하지않는다.(필수값 - innerJoin)
     private Article article; // 게시글 (ID)
     @Setter @Column(nullable = false, length = 500) private String content; // 본문
 
     protected ArticleComment() {}
 
-    private ArticleComment(Article article, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, String content) {
         this.article = article;
+        this.userAccount = userAccount;
         this.content = content;
     }
 
-    public static ArticleComment of(Article article, String content) {
-        return new ArticleComment(article, content);
+    public static ArticleComment of(Article article, UserAccount userAccount, String content) {
+        return new ArticleComment(article, userAccount, content);
     }
 
     @Override
